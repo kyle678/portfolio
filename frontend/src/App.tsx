0c2e474524bf75069e25ce390dd43cdb7b95f450
Kyle678 <73669189+Kyle678@users.noreply.github.com>
2026-06-25 20:24:17 -0500

    c

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>RAM</code>
+            Edit <code>src/App.tsx</code> and save to test <code>RAMD</code>
           </p>
         </div>
         <button
